e3fca105ec681fc2ca8edfa1cfa28c914917e3be
Will Fyson <will.fyson@cosector.com>
2024-01-08 16:23:14 +0000

    Update plugin to use ROR API rather than Crossref, with associated scripts to migrate existing DOIs to RORs and to populate RORs based on names/acronyms

--- lib/plugins/EPrints/FundersAdvanced/Utils.pm
+++ lib/plugins/EPrints/FundersAdvanced/Utils.pm
@@ -2,6 +2,50 @@ package EPrints::FundersAdvanced::Utils;
 
 use strict;
 
+sub ror_query
+{
+	my( $repo, $name ) = @_;
+
+    # build the request
+    my $url = "https://api.ror.org/organizations";
+
+    # add the name to the query
+    $name =~ tr/ /+/;
+    $url .= "?query.advanced=name:$name+OR+acronyms:$name";
+
+    my $ua = LWP::UserAgent->new;
+
+    my $archive_name = $repo->phrase( "archive_name" );
+    $archive_name = $repo->config( "funders_advanced", "archive_name" ) if defined $repo->config( "funders_advanced", "archive_name" );
+
+    # User-Agent header for politeness
+    my $ua_header = $archive_name. " ( " . $repo->config( "host" ) . "; mailto:" . $repo->config( "adminemail" ) . ")";
+
+    my $headers = HTTP::Headers->new(
+        'Accept' => 'application/json',
+        'User-Agent' => $ua_header,
+    );
+
+    my $req =  HTTP::Request->new(
+        GET => $url,
+        $headers,
+    );
+
+    my $res = $ua->request( $req );
+    if( $res->is_success )
+    {
+        my $json = JSON->new->utf8;
+        my $content = $json->decode( $res->content );
+        return $content;
+    }
+    else
+    {
+        $repo->log( "ROR lookup failed: " . $res->code . ", " . $res->content );
+        return 0;
+    }    
+}
+
+
 sub crossref_query
 {
 	my( $repo, $name ) = @_;
